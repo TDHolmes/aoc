0ceacdad26df0d1ba9f815506cc574e7224b63d4
Tyler Holmes <tyler@holmesengineering.com>
2021-12-19 14:54:55 -0800

    pivot from bins to libs so we can do cargo test

--- aoc-2021/src/bin/day_18.rs
+++
@@ -1,6 +0,0 @@
-//! AOC Day xx
-// use aoc_2021;
-
-fn main() {
-    println!("Hello, world");
-}
---
+++ aoc-2021/src/day_21.rs
@@ -0,0 +1,10 @@
+//! AOC Day xx
+// use aoc_2021;
+
+#[cfg(test)]
+mod test {
+    #[test]
+    fn example_test() {
+        assert_eq!(1 + 1, 2);
+    }
+}

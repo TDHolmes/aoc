c65c57a2baaf737a2c233a5ac79c3789ea0f23da
Tyler Holmes <tyler@holmesengineering.com>
2021-12-19 17:59:13 -0800

    further tweak of the template

--- aoc-2021/src/day_02.rs
+++ aoc-2021/src/day_02.rs
@@ -1,6 +1,8 @@
 //! AOC Day xx
 // use aoc_2021;
 
+const EXAMPLE_INPUT: &str = "forward 5\ndown 5\nforward 8\nup 3\ndown 8\nforward 2\n";
+
 fn part_one(_input: &str) -> i32 {
     let mut result = 0;
     result += 2;
@@ -17,13 +19,13 @@ fn part_two(_input: &str) -> i32 {
 
 #[test]
 fn example_part_one() {
-    let result = part_one("input");
+    let result = part_one(EXAMPLE_INPUT);
     assert_eq!(result, 2);
 }
 
 #[test]
 fn example_part_two() {
-    let result = part_two("input");
+    let result = part_two(EXAMPLE_INPUT);
     assert_eq!(result, 2);
 }
 

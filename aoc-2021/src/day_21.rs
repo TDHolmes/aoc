156bfdc3b33b47726debc43db370c3e631e65cdf
Tyler Holmes <tyler@holmesengineering.com>
2021-12-19 17:33:26 -0800

    rejigger the template

--- aoc-2021/src/day_21.rs
+++ aoc-2021/src/day_21.rs
@@ -1,10 +1,38 @@
 //! AOC Day xx
 // use aoc_2021;
 
-#[cfg(test)]
-mod test {
-    #[test]
-    fn example_test() {
-        assert_eq!(1 + 1, 2);
-    }
+fn part_one(_input: &str) -> i32 {
+    let mut result = 0;
+    result += 2;
+
+    result
+}
+
+fn part_two(_input: &str) -> i32 {
+    let mut result = 0;
+    result += 2;
+
+    result
+}
+
+#[test]
+fn example_part_one() {
+    let result = part_one("input");
+    assert_eq!(result, 2);
+}
+
+#[test]
+fn example_part_two() {
+    let result = part_two("input");
+    assert_eq!(result, 2);
+}
+
+#[test]
+fn test_part_one() {
+    assert_eq!(1 + 1, 2);
+}
+
+#[test]
+fn test_part_two() {
+    assert_eq!(1 + 1, 2);
 }
